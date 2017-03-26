2636b7552ef5d24d1e84f65a7ab161f491d6a1e4
Alexander Ferenets <ist.kazan@gmail.com>
2017-03-26 23:00:40 +0300

    Removed Bookstore to another repository.

--- Bookstore/src/main/java/ru/kpfu/bookstore/repositories/PublishingHouseRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.bookstore.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.kpfu.bookstore.models.PublishingHouse;
-
-@Repository
-public interface PublishingHouseRepository  extends CrudRepository<PublishingHouse, Integer>{}
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Kazan Federal University: http://kpfu.ru/
   
   
 Be aware! Code here is just examples for Programming classes. It is only for studying purposes: this code shows some Spring etc functionality and some of it can contain bugs.
+
