ab8e6b906b9aedc15d499bf5a6ddcbf948d180a2
Alexander Ferenets <ist.kazan@gmail.com>
2021-02-24 14:59:58 +0300

    Removed bug_task from converters

--- spring_converters/bug_task/src/main/java/ru/kpfu/springconverterstask/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.springconverterstask.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.kpfu.springconverterstask.models.Book;
-
-@Repository
-public interface BookRepository  extends CrudRepository<Book, Integer>{}
--- spring_converters/bug_task/src/main/java/ru/kpfu/springconverterstask/repositories/PublishingHouseRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.springconverterstask.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.kpfu.springconverterstask.models.PublishingHouse;
-
-@Repository
-public interface PublishingHouseRepository  extends CrudRepository<PublishingHouse, Integer>{}
